bfc3360b304b7b1c5b04beedd55a98bf76672830
Jordan Whitten <jordanwa1947@gmail.com>
2019-12-06 10:41:34 -0700

    Revert back to adding each event lister manually

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
-const emojis = document.querySelectorAll('button')
+const happyEmoji = document.getElementById('happy-emoji');
+const sillyEmoji = document.getElementById('silly-emoji');
+const cryingEmoji = document.getElementById('crying-emoji');
+
 const emojiMessage = document.getElementById('emoji-message');
-loopEmojis();
-// happyEmoji.addEventListener('click', function () {
-//   const responses = ["You go!", "Your smile just cheered me up!", "YAY!"];
-//   let response = generateResponse(responses, 3);
-//   emojiMessage.innerText = response;
-// })
-//
-// sillyEmoji.addEventListener('click', function () {
-//   const responses = ["Interesting response", "Sounds ... good?", "Me too."];
-//   let response = generateResponse(responses, 3);
-//   emojiMessage.innerText = response;
-// })
-//
-// cryingEmoji.addEventListener('click', function () {
-//   const responses = ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"];
-//   let response = generateResponse(responses, 4);
-//   emojiMessage.innerText = response;
-// })
 
-function loopEmojis () {
-  const responses = [["You go!", "Your smile just cheered me up!", "YAY!"],
-  ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"],
-  ["Interesting response", "Sounds ... good?", "Me too."]]
+happyEmoji.addEventListener('click', function () {
+  const responses = ["You go!", "Your smile just cheered me up!", "YAY!"];
+  let response = generateResponse(responses, 3);
+  emojiMessage.innerText = response;
+})
 
-  for (let i = 0; i < emojis.length; i++) {
-    emojiOnClick(emojis[i], responses[i]);
-  }
-}
+sillyEmoji.addEventListener('click', function () {
+  const responses = ["Interesting response", "Sounds ... good?", "Me too."];
+  let response = generateResponse(responses, 3);
+  emojiMessage.innerText = response;
+})
 
-function emojiOnClick (emoji, responses) {
-  emoji.addEventListener('click', function () {
-    let response = generateResponse(responses, responses.length);
-    emojiMessage.innerText = response;
-  })
-}
+cryingEmoji.addEventListener('click', function () {
+  const responses = ["Do you want to talk?", "Keep your head up", "We can cry together!", "What can I do?"];
+  let response = generateResponse(responses, 4);
+  emojiMessage.innerText = response;
+})
 
 function generateResponse(responses, numberOfResponses) {
   let i = Math.floor(Math.random() * numberOfResponses);
